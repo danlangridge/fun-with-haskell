7a037bb5d39e0ee36fc77ad08f0c21d224ebe0d5
Dan <dlangridge@gmail.com>
2014-08-24 20:01:31 -0700

    added Fibonacci haskell example

--- play.hs
+++ play.hs
@@ -6,13 +6,24 @@ f x y = x*x + y*y
 g :: Num a => a -> a -> a -> a
 g v p t = t*v + p
 
+v :: Int
+v = 1
+p :: Int
+p = 2
+t :: Int
+t = 3	
+
+even n = mod n 2 == 0
+
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib(n-2)
+
 main :: IO ()
 main = do 
-	print "Hello, World!"
-	let x = 1
+	print "Hello, World!"	
 	name <- getLine
 	print ("Hello " ++ name ++ "!")
 	print (f 2 3)
-	print ("At time " ++ "1" ++ " At Starting Position " ++ "2" ++ " with Velocity " ++ "3")
-	print ("The new position is ")
-	print (g 3 2 1)
+	print (g v p t)
+	print (fib 20)
